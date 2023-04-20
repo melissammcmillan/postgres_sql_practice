585554495d93832130d01f79f6e6e01755cce7a2
melissa mcmillan <melmmac2013@gmail.com>
2023-04-19 19:03:04 -0600

    updating with new code

--- code/owners_pets_code.sql
+++ code/owners_pets_code.sql
@@ -56,10 +56,42 @@ ALTER COLUMN last_name TYPE VARCHAR(50);
 
 SELECT * FROM owners;
 
+-- challenge for Section 4 learnings
 
+-- insert data into the owners table
+-- we don't need to include the id column in the INSERT clause, but I did anyway
+-- we don't need to include the id # in the VALUES, but I did anyway
+SELECT * FROM owners;
 
+INSERT INTO owners (id, first_name, last_name, city, state, email)
+VALUES (1, 'Samuel', 'Smith', 'Boston', 'MA', 'samsmith@gmail.com'),
+(2, 'Emma', 'Johnson', 'Seattle', 'WA', 'emjohnson@gmail.com'),
+(3, 'John', 'Oliver', 'New York', 'NY', 'johnoliver@gmail.com'),
+(4, 'Olivia', 'Brown', 'San Francisco', 'CA', 'oliviabrown@gmail.com'),
+(5, 'Simon', 'Smith', 'Dallas', 'TX', 'sismith@gmail.com'),
+(7, null, 'Maxwell', null, 'CA', 'lordmaxwell@gmail.com');
 
+-- insert data into the pets table
+SELECT * FROM pets;
 
+INSERT INTO pets(id, species, full_name, age, owner_id)
+VALUES (1, 'Dog', 'Rex', 6, 1),
+(2, 'Rabbit', 'Fluffy', 2, 5),
+(3, 'Cat', 'Tom', 8, 2),
+(4, 'Mouse', 'Jerry', 2, 2),
+(5, 'Dog', 'Biggles', 4, 1),
+(6, 'Tortoise', 'Squirtle', 42, 3);
+
+-- update the rabbit Fluffly's age to 3
+
+UPDATE pets
+SET age = 3
+WHERE id = 2;
+
+-- delete Mr Maxwell from the owners table
+
+DELETE FROM owners
+WHERE id = 7;
 
 
 
--- code/test_database_code.sql
+++ code/test_database_code.sql
@@ -95,7 +95,66 @@ VALUES ('value1', 'value2', 'value3');
 INSERT INTO examples(first_name, last_name, email, nationality, age)
 VALUES ('David', 'Mitchell', 'dmitch@gmail.com', 'GBR', 43);
 
+-- now we will update data in a table
+/*
 
+UPDATE tablename
+SET columnname = 'newvalue'
+WHERE primarykeycolumnname = 'value';
+
+*/
+
+-- change David Mitchells email address
+-- best to use the primary key column name in the WHERE clause
+-- so that the entire column doesn't get replaced with the SET clause
+
+UPDATE examples
+SET email = 'davidmitchell@gmail.com'
+WHERE example_id = 1;
+
+SELECT * FROM examples;
+
+-- since we update the david mitchell row, it is now at the bottom of the table
+
+-- to update multiple rows at once
+-- this will replace all the USA entries in the nationality column to CAN(ada)
+
+UPDATE examples
+SET nationality = 'CAN'
+WHERE nationality = 'USA';
+
+-- we can also update multiple columns in a single row
+-- we will update Jim Burr to be named James and fix his age from 54 to 55
+
+UPDATE examples
+SET first_name = 'James', age = 55
+WHERE example_id = 5;
+
+-- Deleting data from a table
+/* 
+
+DELETE FROM tablename
+WHERE columnname = 'value';
+
+*/
+
+-- this will delete one row of data
+
+DELETE FROM examples
+WHERE example_id = 2;
+
+SELECT * FROM examples;
+
+-- if you wanted to delete all the British people from the table, use a WHERE clause
+-- that the rows have in common
+
+DELETE FROM examples
+WHERE nationality = 'GBR';
+
+-- if you want to delete all the data from a table at once,
+-- don't use a WHERE clause
+
+DELETE FROM examples; 
 
 
 
