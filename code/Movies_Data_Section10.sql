503c76b46580d0b212f515816b591f80874a2e22
melissa mcmillan <melmmac2013@gmail.com>
2023-05-18 06:07:20 -0600

    now practicing string functions

---
+++ code/Movies_Data_Section10.sql
@@ -0,0 +1,13 @@
+-- Practicing String Functions- Section 10
+
+-- Remember we already learned the CONCAT function that concatenated strings together
+
+-- First we will learn how to clone or copy a database so we don't mess up the original
+
+-- I am now using my movie_data_clone database. Note that in order to use the old
+-- database as a template, I had to right click on the name of the old database and
+-- select "Disconnect from Database". Otherwise PGAdmin wouldn't let me use it as a
+-- template.
+
+
+
