ff1fc14d8bdba7c61e87589992b84a0cafffa23c
melissa mcmillan <melmmac2013@gmail.com>
2023-06-08 05:55:00 -0600

    final results for fixtures sql problem

--- challenge_assignment/Futbol_Challenge_Code.sql
+++ challenge_assignment/Futbol_Challenge_Code.sql
@@ -152,8 +152,8 @@ SELECT * FROM intertable;
 
 -- Now I need to create the standings table which will get populated using the info from intertable and fixtures
 
--- Create the standings table that will be the final answer
-CREATE TABLE standings (
+-- Create the initial standings table that I will use to create the final answer
+CREATE TABLE init_standings (
 
 	TeamName VARCHAR(30),
 	GamesPlayed INTEGER,
@@ -165,12 +165,12 @@ CREATE TABLE standings (
 	Points INTEGER
 );
 
-SELECT * FROM standings;
+SELECT * FROM init_standings;
 
 SELECT * FROM fixtures;
 
--- Populate the standings table from intertable
-INSERT INTO standings (
+-- Populate the initial standings table from intertable
+INSERT INTO init_standings (
 SELECT i.teamname, 
 	i.homewins + i.homelosses + i.homedraws + i.awaywins + i.awaylosses + i.awaydraws AS gamesplayed,
 	i.homewins + i.awaywins AS wins, 
@@ -184,242 +184,30 @@ SELECT i.teamname,
 	
 FROM intertable i);
 
-SELECT * FROM standings;
+SELECT * FROM init_standings;
 
--- test some code here
-DROP TABLE testtable;
-
-CREATE TABLE testtable AS
-SELECT * FROM standings
+-- Create the final standings table
+CREATE TABLE standings AS
+SELECT * FROM init_standings
 ORDER BY points DESC;
 
-SELECT * FROM testtable;
+SELECT * FROM standings;
 
-ALTER TABLE testtable
+-- Create the position column using the points column
+ALTER TABLE standings
 ADD COLUMN position serial unique;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-SELECT teamname,
-	CONCAT_WS(':', SUM(homewingoalscored, homelossgoalsscored, homedrawgoalscored, awaywingoalsscored, awaylossgoalsscored, awaydrawgoalsscored), 
-			 SUM(homewingoalsreceived, homelossgoalsreceived, homedrawgoalsreceived, awaywingoalsreceived, awaylossgoalsreceived, awaydrawgoalsreceived)) AS scored_received
-FROM intertable;
-
--- this adds the homewins and awaywins together to make the total column called wins
-SELECT teamname, homewins, awaywins, homewins + awaywins AS wins
-FROM intertable;
-
--- can we use SUM() for this; NO- SUM is an aggregate function, and we would need to use a group by
-SELECT teamname, homewins, awaywins, SUM(homewins,awaywins) AS wins
-FROM intertable;
-	
-SELECT i.teamname, SUM(i.homewins, i.homelosses, i.homedraws, i.awaywins, i.awaylosses, i.awaydraws) AS gamesplayed,
-	SUM(i.homewins, i.awaywins) AS wins, SUM(i.homedraws, i.awaydraws) AS draws, SUM(i.homelosses, i.awaylosses) AS losses,
-	CONCAT_WS(':', SUM(4,3), 
-			 SUM(2,3)) AS scored_received
-FROM intertable i;
-
--- count the number of games played by each team and place in the
--- standings table
-SELECT teamname, COUNT(teamname) FROM standings
-JOIN fixtures ON teamname.standings = hometeam.fixtures
-GROUP BY teamname;
-
--- Give me the hometeams where the home team was the winner
-SELECT round, hometeam FROM fixtures
-WHERE hometeamscore > awayteamscore
-ORDER BY round;
-
--- Put a 1 next to the hometeams that won the match
-SELECT round, hometeam, COUNT(hometeam) FROM fixtures
-WHERE hometeamscore > awayteamscore
-GROUP BY round, hometeam
-ORDER BY round;
-
-
--- join on team name on teamname = hometeam or away team
--- 
-
--- 
--- INSERT INTO standings (gamesplayed)
-SELECT COUNT(DISTINCT(round)) FROM fixtures;
-
-INSERT INTO standings(gamesplayed)
-SELECT (SELECT COUNT(DISTINCT(round))) FROM fixtures;
-
-INSERT INTO standings(gamesplayed)
-SELECT COUNT(DISTINCT(round)) FROM fixtures;
-
-INSERT INTO standings(gamesplayed)
-SELECT t.total_games FROM (SELECT COUNT(DISTINCT(round)) AS total_games FROM fixtures) t;
-
-
-SELECT SUM(COUNT(hometeam), COUNT(awayteam)), * FROM fixtures;
-
-SELECT hometeam FROM fixtures
-UNION
-SELECT awayteam FROM fixtures;
-
-SELECT COUNT(DISTINCT(round)) FROM fixtures
-WHERE hometeam or awayteam = 
-(SELECT teamname FROM standings);
-
-SELECT COUNT(DISTINCT(round)) FROM fixtures
-WHERE 
-
-SELECT hometeam FROM fixtures
-GROUP BY round;
-
-SELECT * FROM fixtures;
+-- Check and deliver the final answer
 SELECT * FROM standings;
 
-SELECT ROW_NUMBER() OVER(PARTITION BY round), *
-FROM fixtures;
-
-
--- Plan of attack: 
--- 1. Find the wins, draws, and losses and add them into standings
-SELECT * FROM standings;
-SELECT * FROM fixtures;
-
--- get the homewins for standings
-SELECT s.teamname, COUNT(f.hometeamscore) AS homewincount FROM standings s
-LEFT JOIN fixtures f ON f.hometeam = s.teamname
-WHERE f.hometeamscore > f.awayteamscore
-GROUP BY s.teamname
-ORDER BY teamname;
-
--- get the awaywins for standings
-SELECT s.teamname, COUNT(f.awayteamscore) AS awaywincount FROM standings s
-LEFT JOIN fixtures f ON f.awayteam = s.teamname
-WHERE f.hometeamscore < f.awayteamscore
-GROUP BY s.teamname
-ORDER BY teamname;
-
-INSERT INTO standings(gamesplayed)
-VALUES (SELECT COUNT(DISTINCT(round)) FROM fixtures f
-LEFT JOIN ;
 
 
 
-SELECT teamname,
-
-	(SELECT COUNT(f.hometeamscore) FROM standings s
-	LEFT JOIN fixtures f ON f.hometeam = s.teamname
-	WHERE f.hometeamscore > f.awayteamscore
-	GROUP BY s.teamname) AS homewins, 
-	
-	(SELECT COUNT(f.awayteamscore) FROM standings s
-	LEFT JOIN fixtures f ON f.awayteam = s.teamname
-	WHERE f.hometeamscore < f.awayteamscore
-	GROUP BY s.teamname) AS awaywins
-
-FROM standings
-GROUP BY teamname;
-
-SELECT * FROM standings;
-SELECT * FROM fixtures;
-		
--- Trying PARTITION here instead
-SELECT hometeam, COUNT(hometeamscore) OVER () as totalhomewins
-FROM fixtures
-WHERE hometeamscore > awayteamscore;
-		
-
-SELECT 'homewins', hometeam, COUNT(hometeamscore) AS homewins FROM fixtures
-WHERE hometeamscore > awayteamscore
-GROUP BY hometeam
-UNION
-SELECT 'awaywins', awayteam, COUNT(awayteamscore) AS awaywins FROM fixtures
-WHERE hometeamscore < awayteamscore
-GROUP BY awayteam;
-
-SELECT 'homewins', hometeam, COUNT(hometeamscore) AS homewins FROM fixtures
-WHERE hometeamscore > awayteamscore
-GROUP BY hometeam;
 
 
-SELECT teamname, homewins + awaywins AS totalwins,
-	(SELECT COUNT(hometeamscore) FROM fixtures
-	WHERE hometeamscore > awayteamscore
-	GROUP BY hometeam) AS homewins, 
-	(SELECT awayteam, COUNT(awayteamscore) FROM fixtures
-	WHERE hometeamscore < awayteamscore
-	GROUP BY awayteam) AS awaywins
-	FROM standings;
-
---
-SELECT position, teamname, homecount + awaycount AS totalcount,
-(SELECT COUNT(DISTINCT(f.round)) FROM fixtures f
-	JOIN fixtures f ON f.hometeam = s.teamname
-	GROUP BY s.teamname) AS homecount,  
-(SELECT s.teamname, COUNT(DISTINCT(f.round)) FROM standings s
-	JOIN fixtures f ON f.awayteam = s.teamname
-	GROUP BY s.teamname) AS awaycount
-FROM standings
-GROUP BY position, teamname;
-
---
-INSERT INTO standings(gamesplayed)
-SELECT teamname, 
-	(SELECT COUNT(DISTINCT(round)) FROM fixtures) AS gamecount
-FROM standings;
-
-SELECT 
-	(SELECT COUNT(DISTINCT(round)) FROM fixtures) AS gamecount
-FROM standings;
-
-DELETE FROM standings
-WHERE teamname is null;
-
-SELECT * FROM standings;
-
-SELECT COUNT(DISTINCT(hometeam)) FROM fixtures;
-SELECT COUNT(DISTINCT(awayteam)) FROM fixtures;
-
-SELECT 4 + 5;
-
-SELECT s.position, s.teamname, COUNT(DISTINCT(f.round)) FROM standings s
-JOIN fixtures f ON f.hometeam = s.teamname
-GROUP BY s.position, s.teamname;
-
-SELECT COUNT(DISTINCT(f.round)) AS homecount FROM standings s
-	JOIN fixtures f ON f.hometeam = s.teamname;
-	
-SELECT COUNT(DISTINCT(f.round)) FROM fixtures f;
-	JOIN fixtures f ON f.hometeam = s.teamname
-	GROUP BY s.teamname
-
-SELECT f.hometeam, COUNT(f.hometeamscore), s.wins FROM fixtures f
-JOIN standings s ON s.teamname = f.hometeamscore
-WHERE f.hometeamscore > f.awayteamscore
-GROUP BY f.hometeam, s.wins;
 
 
--- 2. Then total the wins, draws, and losses to the gamesplayed column, should equal 38
--- 3. Then calculate the scored_received, goaldifference, and points columns
--- 4. Calculate the positions and fill them in
+
 
 
 
